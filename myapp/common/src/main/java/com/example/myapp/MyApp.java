6c9ab84f2ee7ca9ddc6ed460ecce86914632f861
Dawn <yokee2019@gmail.com>
2023-11-10 20:57:16 -0800

    yay

--- myapp/common/src/main/java/com/example/myapp/MyApp.java
+++ myapp/common/src/main/java/com/example/myapp/MyApp.java
@@ -66,6 +66,7 @@ public class MyApp extends com.codename1.system.Lifecycle {
             this.mainApp = mainApp;
 
             getToolbar().addCommandToSideMenu("Tasks", null, e -> showTab("Tasks"));
+            getToolbar().addCommandToSideMenu("Character Selection", null, e -> showCharacterSelection());
             getToolbar().addCommandToSideMenu("Character Selection", null, e -> showTab("Character Selection"));
             getToolbar().addCommandToSideMenu("Character Status", null, e -> showTab("Character Status"));
             getToolbar().addCommandToSideMenu("Achievements", null, e -> showTab("Achievements"));
@@ -79,6 +80,19 @@ public class MyApp extends com.codename1.system.Lifecycle {
 
         private void logout() {
             mainApp.showSignInForm();
+        }
+
+        private void showCharacterSelection(){
+            CharacterSelectionPage characterSelectionPage = new CharacterSelectionPage();
+            characterSelectionPage.show(); 
+        }
+    }
+
+    public class CharacterSelectionPage extends Form {
+        public CharacterSelectionPage(){
+            super("Character Selection", BoxLayout.y());
+
+            //add character selection page components and logic here
         }
     }
 
