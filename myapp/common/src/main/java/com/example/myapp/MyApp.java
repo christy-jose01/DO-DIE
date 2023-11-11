b810289192caa5d51abc3fedb17e149a7fa204dd
bruhitsyandrea <lisachen20000226@gmail.com>
2023-11-11 03:57:34 -0800

    wk

--- myapp/common/src/main/java/com/example/myapp/MyApp.java
+++ myapp/common/src/main/java/com/example/myapp/MyApp.java
@@ -2,14 +2,32 @@ package com.example.myapp;
 
 import com.codename1.components.SpanLabel;
 import com.codename1.ui.Button;
+import com.codename1.ui.Component;
 import com.codename1.ui.Container;
 import com.codename1.ui.Dialog;
 import com.codename1.ui.FontImage;
 import com.codename1.ui.Form;
+import com.codename1.ui.Graphics;
 import com.codename1.ui.TextComponent;
 import com.codename1.ui.TextComponentPassword;
-import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.geom.Dimension;
 import com.codename1.ui.layouts.BoxLayout;
+import com.codename1.ui.layouts.BorderLayout;
+import com.codename1.ui.plaf.Style;
+import com.codename1.ui.util.Resources;
+import com.codename1.ui.util.UITimer;
+import com.codename1.ui.plaf.Border;
+import com.codename1.ui.Font;
+
+
+
+
+import javafx.scene.control.ProgressBar;
+
+// Import for ProgressBar
+//import com.codename1.ui.spinner.ProgressBar;
+
+// Import for Label
 import com.codename1.ui.Label;
 
 public class MyApp extends com.codename1.system.Lifecycle {
@@ -73,7 +91,7 @@ public class MyApp extends com.codename1.system.Lifecycle {
             getToolbar().addCommandToSideMenu("Character Selection", null, e -> showCharacterSelection());
             getToolbar().addCommandToSideMenu("Character Selection", null, e -> showTab("Character Selection"));
             getToolbar().addCommandToSideMenu("Character Status", null, e -> showTab("Character Status"));
-            getToolbar().addCommandToSideMenu("Achievements", null, e -> showAchievementsPage());
+            getToolbar().addCommandToSideMenu("Weekly Summary", null, e -> showWeeklySummary());
             getToolbar().addCommandToSideMenu("Settings", null, e -> showTab("Settings"));
             getToolbar().addCommandToSideMenu("Logout", null, e -> logout());
         }
@@ -101,9 +119,10 @@ public class MyApp extends com.codename1.system.Lifecycle {
             settingsPage.show();
         }
 
-        private void showAchievementsPage() {
-            AchievementsPage achievementsPage = new AchievementsPage();
-            achievementsPage.show();
+        
+        private void showWeeklySummary() {
+            WeeklySummaryPage weeklySummaryPage = new WeeklySummaryPage();
+            weeklySummaryPage.show();
         }
     }
 
@@ -314,29 +333,106 @@ public class MyApp extends com.codename1.system.Lifecycle {
         }
     }
 
-    public class AchievementsPage extends Form {
+    public class CustomProgressBar extends Container {
 
-        public AchievementsPage() {
-            super("Achievements", BoxLayout.y());
+        private float progress;
+
+        public CustomProgressBar() {
+            this.progress = 0.5f; // Set the initial progress value
+            getAllStyles().setBgColor(0xFFFFFF); // Set the background color
+            getAllStyles().setBorder(Border.createLineBorder(1, 0x000000)); // Set the border color and size
+        }
+
+        public float getProgress() {
+            return progress;
+        }
+
+        public void setProgress(float progress) {
+            if (progress >= 0 && progress <= 1) {
+                this.progress = progress;
+                repaint(); // Trigger repaint to update the progress bar
+            }
+        }
+
+        @Override
+        protected void paintBackground(Graphics g) {
+            super.paintBackground(g);
+
+            // Calculate the width of the filled area based on the progress
+            int fillWidth = (int) (getWidth() * progress);
+
+            // Set the color of the filled area
+            g.setColor(0x007AFF); // You can customize the color here
+
+            // Draw the filled area
+            g.fillRect(getX(), getY(), fillWidth, getHeight());
+        }
+
+        @Override
+        protected Dimension calcPreferredSize() {
+            return new Dimension(260, 37); // Set the preferred size of your progress bar
+        }
+    }
+
+    public class WeeklySummaryPage extends Form {
+
+        public WeeklySummaryPage() {
+            super("Weekly Summary", BoxLayout.y());
     
-            // Add achievements content here
-            addAchievement("Completed Quest 1");
-            addAchievement("Reached Level 10");
-            addAchievement("Earned 1000 Gold");
+            // Days of the week
+            String[] days = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
+    
+            for (String day : days) {
+                // Day label
+                Label dayLabel = new Label(day);
+                dayLabel.getAllStyles().merge(createDayLabelStyle());
+                add(dayLabel);
+    
+                // Custom progress bar
+                CustomProgressBar customProgressBar = new CustomProgressBar();
+                add(customProgressBar);
+    
+                // Spacing between days
+                add(createSpacer());
+            }
     
             // Add a back button to return to the previous form
             getToolbar().setBackCommand("Back", e -> showPreviousForm());
         }
     
-        private void addAchievement(String achievementText) {
-            Label achievementLabel = new Label(achievementText);
-            add(achievementLabel);
+        private Style createDayLabelStyle() {
+            Style dayLabelStyle = new Style();
+            dayLabelStyle.setBgColor(0xFFFFFF);
+            dayLabelStyle.setFgColor(0x000000);
+            dayLabelStyle.setBorder(Border.createLineBorder(1, 0x000000));
+            //dayLabelStyle.setFont(Font.createTrueTypeFont("Inter", "Inter-Regular.ttf", Font.STYLE_PLAIN, 20));
+            //dayLabelStyle.setFont(Font.createTrueTypeFont("Inter", "Inter-Regular.ttf").derive(Font.STYLE_PLAIN, 20));
+            dayLabelStyle.setFont(Font.createSystemFont(Font.FACE_SYSTEM, Font.STYLE_PLAIN, Font.SIZE_MEDIUM));
+
+
+
+            return dayLabelStyle;
+        }
+    
+        private Label createSpacer() {
+            Label spacer = new Label();
+            spacer.getAllStyles().merge(createSpacerStyle());
+            return spacer;
+        }
+    
+        private Style createSpacerStyle() {
+            Style spacerStyle = new Style();
+            spacerStyle.setBgColor(0xFFFFFF);
+            spacerStyle.setBgTransparency(255);
+            return spacerStyle;
         }
     
         private void showPreviousForm() {
             showBack();
         }
     }
+    
+    
     
     //Christy's section: Tasks
 
