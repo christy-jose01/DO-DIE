147c078963cfb874e2bf121b08f4eb5ce574bddf
Dawn <yokee2019@gmail.com>
2023-11-10 18:22:20 -0800

    lets get strawberry madness

---
+++ README.md
@@ -0,0 +1,19 @@
+# DO/DIE
+Do/Die
+
+# Description:
+This is an app that helps users keep track of their tasks throughout the day with the help of their virtual pet. The user keeps the pet alive by finishing their tasks by their due date. If the user fails to finish a task, they starve the pet. If the user doesn't finish any task, their pet will die. The goal of this app is to make the users accountable for their tasks and to inspire them to complete them in a fun manner. This app does an analysis to help the user get better at completing their tasks.
+
+# Features
+- When first logging onto the app, the user can select their pet and customize it by color and personality type (happy or aggressive) and name it.
+- On the Lock screen: The pet's health is displayed.
+- Once the user has created the pet, they will be guided to create their tasks.
+    - REQUIRED: The users will have to prioritize their tasks based on their importance and their urgency (The Eisenhower Matrix)
+    - Tasks will be weighted based on their priority
+    - REQUIRED: The users will also get to categorize their tasks (health, school, social, life (chores, errands, gifting), beauty).
+    - Tasks will have a deadline. If the tasks do not have a specified deadline, it will be due at 11:59 pm. 
+- When the user completes their task, their pet will be fed, happy, and alive.
+- When the user fails one task, their pet will be starved and will complain (before they die)
+- Based on the personality, the pet will give motivation or threat (customized speech) 10 minutes before their task is due.
+
+  
---
+++ myapp/common/src/main/java/com/example/myapp/MyApp.java
@@ -0,0 +1,84 @@
+package com.example.myapp;
+
+import com.codename1.ui.Button;
+import com.codename1.ui.Dialog;
+import com.codename1.ui.FontImage;
+import com.codename1.ui.Form;
+import com.codename1.ui.TextComponent;
+import com.codename1.ui.TextComponentPassword;
+import com.codename1.ui.layouts.BoxLayout;
+
+public class MyApp extends com.codename1.system.Lifecycle {
+    private Form signInForm;
+
+    @Override
+    public void runApp() {
+        signInForm = new Form("Sign in", BoxLayout.y());
+
+        TextComponent usernameCaption = new TextComponent();
+        usernameCaption.label("username");
+        //TextArea usernameSpace = new TextArea();
+
+        TextComponentPassword passwordField = new TextComponentPassword();
+        passwordField.label("password");
+       // TextArea passwordSpace = new TextArea();
+
+        signInForm.add(usernameCaption);
+       // signInForm.add(usernameSpace);
+        signInForm.add(passwordField);
+       // signInForm.add(passwordSpace);
+
+        Button signInButton = new Button("Sign in");
+        signInForm.add(signInButton);
+        signInButton.addActionListener(e -> signIn(usernameCaption.getText(), passwordField.getText()));
+
+        signInForm.getToolbar().addMaterialCommandToSideMenu("Hello Command", FontImage.MATERIAL_CHECK, 4, e -> hello());
+        signInForm.show();
+    }
+
+    private void signIn(String username, String password) {
+        if (isValidCredentials(username, password)) {
+            HomePage homePage = new HomePage(this);
+            homePage.show();
+        } else {
+            Dialog.show("Invalid Credentials", "Please check your username and password", "OK", null);
+        }
+    }
+
+    private boolean isValidCredentials(String username, String password) {
+        return !username.isEmpty() && !password.isEmpty();
+    }
+
+    private void hello() {
+        Dialog.show("Sign Up Complete!", "Do you confirm these updates", "Yes", "No");
+    }
+
+    public void showSignInForm() {
+        signInForm.showBack();
+    }
+
+    public class HomePage extends Form {
+
+        private final MyApp mainApp;
+
+        public HomePage(MyApp mainApp) {
+            super("Home Page", BoxLayout.y());
+            this.mainApp = mainApp;
+
+            getToolbar().addCommandToSideMenu("Tasks", null, e -> showTab("Tasks"));
+            getToolbar().addCommandToSideMenu("Character Selection", null, e -> showTab("Character Selection"));
+            getToolbar().addCommandToSideMenu("Character Status", null, e -> showTab("Character Status"));
+            getToolbar().addCommandToSideMenu("Achievements", null, e -> showTab("Achievements"));
+            getToolbar().addCommandToSideMenu("Settings", null, e -> showTab("Settings"));
+            getToolbar().addCommandToSideMenu("Logout", null, e -> logout());
+        }
+
+        private void showTab(String tabName) {
+            Dialog.show("Tab Selected", "You selected the " + tabName + " tab", "OK", null);
+        }
+
+        private void logout() {
+            mainApp.showSignInForm();
+        }
+    }
+}
